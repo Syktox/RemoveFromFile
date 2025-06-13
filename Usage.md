7b5f646854e1377ea6a254c58823a8260b7192c9
syktox <markus.kammerstetter@hotmail.com>
2025-06-13 15:53:09 +0200

    Update

---
+++ Usage.md
@@ -0,0 +1,20 @@
+# File
+## Structure
+For the structure of the file I used this:
+
+----
+FILEPATH 1
+
+FILEPATH 2
+
+FILEPATH 3
+
+EMPTY LINE 
+
+CHAR / SENTENCE to delete
+
+# Input
+As input please give a file that is structured like the file above
+
+# Output
+We directly change the files so the output is in the listed files
---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+
+int main() {
+    std::filesystem::path filePath;
+    bool bMessageToDelete = false;
+    std::string messageToDelete;
+    std::string files;
+
+    std::cout << "Enter the file containing the filepath and the thing that should be removed" << std::endl;
+    // std::cin >> filePath;
+    filePath = "Test.txt";  // Test only
+
+    // Same as ifstream
+    std::fstream file(filePath, std::ios::in);
+
+    std::cout << "Checking if file is valid" << std::endl;
+
+    // Should print out the whole textfile
+    if (file.is_open())
+    {
+        std::cout << "Opened file: " << filePath << std::endl;
+        std::string line;
+
+        while (!file.eof())
+        {
+            getline(file, line);
+            std::cout << line << std::endl;
+
+            // Splitter
+            if (line.empty())
+                bMessageToDelete = true;
+
+            // write message to delete in a buffer
+            if (bMessageToDelete)
+                messageToDelete += line;
+            else
+                files += line;
+        }
+        file.close();
+        std::cout << "Closed file" << std::endl;
+    }
+
+    std::cout << files;
+
+    return 0;
+}
